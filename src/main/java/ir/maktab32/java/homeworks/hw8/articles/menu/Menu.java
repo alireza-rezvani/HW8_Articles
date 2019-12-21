4c5fd4be87ffef04907d7c7d307018dab77dc25b
arfjava <arrfjava@gmail.com>
2019-12-21 17:39:19 +0330

    Menu.java completed

---
+++ src/main/java/ir/maktab32/java/homeworks/hw8/articles/menu/Menu.java
@@ -0,0 +1,13 @@
+package ir.maktab32.java.homeworks.hw8.articles.menu;
+
+public class Menu {
+    public static void display(){
+        System.out.println("+---------------------------------+");
+        System.out.println("|                Menu             |");
+        System.out.println("+---------------------------------+");
+        System.out.println("| 1. Sign In                      |");
+        System.out.println("| 2. Sign Up                      |");
+        System.out.println("| 3. Articles                     |");
+        System.out.println("+---------------------------------+");
+    }
+}
