00f9751dbf33a9ff1b0fd62752235d5d7f6add18
arfjava <arrfjava@gmail.com>
2019-12-21 17:17:37 +0330

    Article.java completed

---
+++ src/main/java/ir/maktab32/java/homeworks/hw8/articles/entities/Article.java
@@ -0,0 +1,30 @@
+package ir.maktab32.java.homeworks.hw8.articles.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@Entity
+public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String title;
+    private String brief;
+    private String content;
+    private Date createDate;
+    private Date lastUpdateDate;
+    private Date publishDate;
+    private Boolean isPublished;
+
+
+    @ManyToOne
+    private User author;
+
+    @ManyToOne
+    private Category category;
+}
