8441ab5032faecd0124067fad55ab97dd8719465
arfjava <arrfjava@gmail.com>
2019-12-22 23:26:23 +0330

    menu edited

--- src/main/java/ir/maktab32/java/homeworks/hw8/articles/menu/Menu.java
+++ src/main/java/ir/maktab32/java/homeworks/hw8/articles/menu/Menu.java
@@ -1,13 +1,31 @@
 package ir.maktab32.java.homeworks.hw8.articles.menu;
 
+import ir.maktab32.java.homeworks.hw8.articles.entities.User;
+import ir.maktab32.java.homeworks.hw8.articles.share.AuthenticationService;
+
 public class Menu {
     public static void display(){
-        System.out.println("+---------------------------------+");
-        System.out.println("|                Menu             |");
-        System.out.println("+---------------------------------+");
-        System.out.println("| 1. Sign In                      |");
-        System.out.println("| 2. Sign Up                      |");
-        System.out.println("| 3. Articles                     |");
-        System.out.println("+---------------------------------+");
+        User signedInUser = AuthenticationService.getInstance().getSignedInUser();
+        if (signedInUser == null) {
+            System.out.println("+---------------------------------+");
+            System.out.println("|                Menu             |");
+            System.out.println("+---------------------------------+");
+            System.out.println("| sign in  --> Sign In            |");
+            System.out.println("| sign up  --> Sign Up            |");
+            System.out.println("| articles --> Articles           |");
+            System.out.println("| view     --> Article Details    |");
+            System.out.println("+---------------------------------+");
+        }
+        else{
+            System.out.println("+---------------------------------+");
+            System.out.println("|                Menu             |");
+            System.out.println("+---------------------------------+");
+            System.out.println("| sign in  --> Sign In            |");
+            System.out.println("| sign up  --> Sign Up            |");
+            System.out.println("| sign out --> Sign Out          |");
+            System.out.println("| articles --> Articles           |");
+            System.out.println("| view     --> Article Details    |");
+            System.out.println("+---------------------------------+");
+        }
     }
 }
