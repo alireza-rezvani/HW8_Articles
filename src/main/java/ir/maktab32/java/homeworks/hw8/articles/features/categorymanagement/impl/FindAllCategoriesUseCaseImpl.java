1bc83d0225035c07c2971e877cefc98ae4f57066
arfjava <arrfjava@gmail.com>
2019-12-23 02:49:38 +0330

    FindAllCategoriesUseCase & FindAllCategoriesUseCaseImpl completed

---
+++ src/main/java/ir/maktab32/java/homeworks/hw8/articles/features/categorymanagement/impl/FindAllCategoriesUseCaseImpl.java
@@ -0,0 +1,24 @@
+package ir.maktab32.java.homeworks.hw8.articles.features.categorymanagement.impl;
+
+import ir.maktab32.java.homeworks.hw8.articles.config.hibernate.HibernateUtil;
+import ir.maktab32.java.homeworks.hw8.articles.entities.Category;
+import ir.maktab32.java.homeworks.hw8.articles.features.categorymanagement.usecase.FindAllCategoriesUseCase;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindAllCategoriesUseCaseImpl implements FindAllCategoriesUseCase {
+    @Override
+    public List<Category> list() {
+        List<Category> result = new ArrayList<>();
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        result = session.createQuery("from Category").list();
+        session.getTransaction().commit();
+        return result;
+    }
+}
---
+++ src/main/java/ir/maktab32/java/homeworks/hw8/articles/features/categorymanagement/usecase/FindAllCategoriesUseCase.java
@@ -0,0 +1,9 @@
+package ir.maktab32.java.homeworks.hw8.articles.features.categorymanagement.usecase;
+
+import ir.maktab32.java.homeworks.hw8.articles.entities.Category;
+
+import java.util.List;
+
+public interface FindAllCategoriesUseCase {
+    List<Category> list();
+}
