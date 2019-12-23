3c8a0f87caaa6c93b7822ecad1713661db64f174
arfjava <arrfjava@gmail.com>
2019-12-23 17:20:46 +0330

    FindArticleByAuthorUseCase & FindArticleByAuthorUseCaseImpl completed

---
+++ src/main/java/ir/maktab32/java/homeworks/hw8/articles/features/articlemanagement/impl/FindArticleByAuthorUseCaseImpl.java
@@ -0,0 +1,24 @@
+package ir.maktab32.java.homeworks.hw8.articles.features.articlemanagement.impl;
+
+import ir.maktab32.java.homeworks.hw8.articles.config.hibernate.HibernateUtil;
+import ir.maktab32.java.homeworks.hw8.articles.entities.Article;
+import ir.maktab32.java.homeworks.hw8.articles.features.articlemanagement.usecase.FindArticleByAuthorUseCase;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+
+import java.util.List;
+
+public class FindArticleByAuthorUseCaseImpl implements FindArticleByAuthorUseCase {
+    @Override
+    public List<Article> list(String author) {
+        List<Article> result;
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        Query query = session.createQuery("from Article where author = " + author);
+        result = query.list();
+        session.getTransaction().commit();
+        return result;
+    }
+}
