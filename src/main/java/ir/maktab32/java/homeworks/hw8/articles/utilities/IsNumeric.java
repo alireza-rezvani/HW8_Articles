24b56d9f4fc3f0fb9022d0fdda3a0e959f955d38
arfjava <arrfjava@gmail.com>
2019-12-24 01:12:10 +0330

    IsNumeric completed

--- src/main/java/ir/maktab32/java/homeworks/hw8/articles/utilities/IsNumeric.java
+++ src/main/java/ir/maktab32/java/homeworks/hw8/articles/utilities/IsNumeric.java
@@ -1,6 +1,6 @@
 package ir.maktab32.java.homeworks.hw8.articles.utilities;
 
-public class isNumeric {
+public class IsNumeric {
     public static boolean run(String string){
         try {
             Long.parseLong(string);
